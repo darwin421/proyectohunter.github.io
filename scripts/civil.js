e0b6b03fd08995c5893da97a36cdf9edbb1ae323
Darwin <darwinmarcani20@gmail.com>
2026-02-09 00:00:59 +0100

    Version 0.2

---
+++ scripts/civil.js
@@ -0,0 +1,23 @@
+function cargarContenido(ruta) {
+    fetch(ruta)
+        .then(respuesta => respuesta.text())
+        .then(html => {
+            document.getElementById("contenido").innerHTML = html;
+                        // Cerrar menú móvil después de seleccionar una opción
+            document.querySelector(".sidebar").classList.remove("activo");
+
+
+        })
+        .catch(error => {
+            document.getElementById("contenido").innerHTML = "<p>Error al cargar el contenido.</p>";
+            console.error("Error cargando contenido:", error);
+        });
+}
+
+// Cargar por defecto la Presentación al entrar a Civil
+document.addEventListener("DOMContentLoaded", () => {
+    cargarContenido("../guias/guiascivil/presentacioncivil.html");
+});
+function toggleMenu() {
+    document.querySelector(".sidebar").classList.toggle("activo");
+}
